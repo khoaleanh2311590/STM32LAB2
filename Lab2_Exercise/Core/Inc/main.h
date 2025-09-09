38a9647540be43d0f21100036aa41c733731a264
LAPTOP-1L7C1U0Q\acer <khoa.leanh@hcmut.edu.vn>
2025-09-09 21:26:19 +0700

    Init for Ex1

--- Lab2_Exercise/Core/Inc/main.h
+++ Lab2_Exercise/Core/Inc/main.h
@@ -58,6 +58,26 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_Pin GPIO_PIN_5
+#define LED_RED_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_6
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_7
+#define EN1_GPIO_Port GPIOA
+#define LED1_Pin GPIO_PIN_0
+#define LED1_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_1
+#define LED2_GPIO_Port GPIOB
+#define LED3_Pin GPIO_PIN_2
+#define LED3_GPIO_Port GPIOB
+#define LED4_Pin GPIO_PIN_3
+#define LED4_GPIO_Port GPIOB
+#define LED5_Pin GPIO_PIN_4
+#define LED5_GPIO_Port GPIOB
+#define LED6_Pin GPIO_PIN_5
+#define LED6_GPIO_Port GPIOB
+#define LED7_Pin GPIO_PIN_6
+#define LED7_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
