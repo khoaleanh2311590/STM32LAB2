1405ad21c9aa5ef3258403bc2464af3929633362
LAPTOP-1L7C1U0Q\acer <khoa.leanh@hcmut.edu.vn>
2025-09-30 18:29:38 +0700

    Update for Ex9

--- Lab2_Exercise/Core/Src/main.c
+++ Lab2_Exercise/Core/Src/main.c
@@ -228,14 +228,15 @@ void clearAllLEDMatrix(){
 const int MAX_LED_MATRIX=8;
 int index_led_matrix=0;
 uint8_t matrix_buffer[8]={
-		0b00011110, // cột 0
-	    0b00100001, // cột 1
-	    0b01000001, // cột 2
-	    0b01000001, // cột 3
-	    0b01111111, // cột 4
-	    0b01000001, // cột 5
-	    0b01000001, // cột 6
-	    0b01000001  // cột 7
+		  0b00011000,
+		  0b00100100,
+		  0b01000010,
+		  0b01000010,
+		  0b01111110,
+		  0b01000010,
+		  0b01000010,
+		  0b01000010
+
 		};
 void clearAllMatrixCOL(){
 	HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, SET);
@@ -258,43 +259,34 @@ void clearAllMatrixROW(){
 	HAL_GPIO_WritePin(ROW7_GPIO_Port, ROW7_Pin, SET);
 }
 void updateLEDMatrix(int index){
-	clearAllMatrixCOL();
-	clearAllMatrixROW();
-	switch(index){
-	case 0:
-		HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, RESET);
-		break;
-	case 1:
-		HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, RESET);
-		break;
-	case 2:
-		HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, RESET);
-		break;
-	case 3:
-		HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, RESET);
-		break;
-	case 4:
-		HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, RESET);
-		break;
-	case 5:
-		HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, RESET);
-		break;
-	case 6:
-		HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, RESET);
-		break;
-	case 7:
-		HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, RESET);
-		break;
-	}
-    if(matrix_buffer[index] & (1<<0)) HAL_GPIO_WritePin(ROW0_GPIO_Port, ROW0_Pin, RESET);
-    if(matrix_buffer[index] & (1<<1)) HAL_GPIO_WritePin(ROW1_GPIO_Port, ROW1_Pin, RESET);
-    if(matrix_buffer[index] & (1<<2)) HAL_GPIO_WritePin(ROW2_GPIO_Port, ROW2_Pin, RESET);
-    if(matrix_buffer[index] & (1<<3)) HAL_GPIO_WritePin(ROW3_GPIO_Port, ROW3_Pin, RESET);
-    if(matrix_buffer[index] & (1<<4)) HAL_GPIO_WritePin(ROW4_GPIO_Port, ROW4_Pin, RESET);
-    if(matrix_buffer[index] & (1<<5)) HAL_GPIO_WritePin(ROW5_GPIO_Port, ROW5_Pin, RESET);
-    if(matrix_buffer[index] & (1<<6)) HAL_GPIO_WritePin(ROW6_GPIO_Port, ROW6_Pin, RESET);
-    if(matrix_buffer[index] & (1<<7)) HAL_GPIO_WritePin(ROW7_GPIO_Port, ROW7_Pin, RESET);
+    // 1. Tắt tất cả cột
+    clearAllMatrixCOL();
+    clearAllMatrixROW();
+
+    // 2. Xuất dữ liệu cho hàng
+    uint8_t data = matrix_buffer[index];
+    HAL_GPIO_WritePin(ROW0_GPIO_Port, ROW0_Pin, (data & (1<<0)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW1_GPIO_Port, ROW1_Pin, (data & (1<<1)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW2_GPIO_Port, ROW2_Pin, (data & (1<<2)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW3_GPIO_Port, ROW3_Pin, (data & (1<<3)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW4_GPIO_Port, ROW4_Pin, (data & (1<<4)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW5_GPIO_Port, ROW5_Pin, (data & (1<<5)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW6_GPIO_Port, ROW6_Pin, (data & (1<<6)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(ROW7_GPIO_Port, ROW7_Pin, (data & (1<<7)) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+
+    // 3. Bật cột hiện tại
+    switch(index){
+        case 0: HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, RESET); break;
+        case 1: HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, RESET); break;
+        case 2: HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, RESET); break;
+        case 3: HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, RESET); break;
+        case 4: HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, RESET); break;
+        case 5: HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, RESET); break;
+        case 6: HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, RESET); break;
+        case 7: HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, RESET); break;
+    }
 }
+
 /*void chuA(int index){
 	updateLEDMatrix(index);
 	clearAllMatrixROW();
